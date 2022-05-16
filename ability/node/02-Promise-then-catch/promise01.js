dd28b45bf6d0dc317be623b2f816f7ed9e3822e6
egg <egg810207@gmail.com>
2022-05-16 16:14:48 +0800

    新增 接力大賽 用.then 持續往下做事情

--- ability/node/02-Promise-then-catch/promise01.js
+++ ability/node/02-Promise-then-catch/promise01.js
@@ -3,9 +3,8 @@ import { mingRunPromise } from './mingRunPromise.js'
 
 const greenBut = document.querySelector('.green')
 
+// 小明自己跑步 隨機成功或失敗 
 greenBut.addEventListener('click', event => {
-  // console.log(event.target);
-  // 小明自己跑步 隨機成功或失敗 
   mingRunPromise(event.target.innerHTML,3000)
   .then((data)=> {
     // 成功訊息 (需要 3 秒)
@@ -13,6 +12,32 @@ greenBut.addEventListener('click', event => {
     clearLi()
   }).catch((err)=> {
     // 失敗訊息 (立即)
+    createLiElement(err)
+    clearLi()
+  });
+})
+
+const blueBut = document.querySelector('.blue')
+
+// 接力大賽 用.then 隨機成功或失敗 
+blueBut.addEventListener('click', event => {
+  mingRunPromise(event.target.innerHTML, 1000)
+  .then((data)=> {
+    createLiElement(data)
+    createLiElement('下一位')
+    return mingRunPromise('阿姨', 3000)
+  })
+  .then((data)=> {
+    createLiElement(data)
+    createLiElement('下一位')
+    return mingRunPromise('爺爺', 500)
+  })
+  .then((data)=> {
+    createLiElement(data)
+    createLiElement('END')
+    clearLi()
+  })
+  .catch((err)=> {
     createLiElement(err)
     clearLi()
   });
