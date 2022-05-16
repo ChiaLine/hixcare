b2d64f49b24701a2f3f48d0ad4de478f7c9b7ed7
egg <egg810207@gmail.com>
2022-05-16 15:58:22 +0800

    引入模組方法 用於更新畫面

--- ability/node/02-Promise-then-catch/promise.js
+++ ability/node/02-Promise-then-catch/promise.js
@@ -1,50 +1,63 @@
-// 流程控制、非同步、異步
-// 問題的起源（當出現非同步時 return 會失效）
+import { createLiElement, clearLi } from './renderPromise.js'
+// console.log( createLiElement, clearLi )
+
+// 問題的起源（當出現非同步時 return 會失效） 流程控制、非同步、異步
+
 function delayedAdd(n1, n2, delayTime){
-  // 建立 promise 物件： new promise(執行函數)
+  // 建立 promise 物件： new promise(執行延遲計算函數)
   return new Promise(function(resolve, reject){
+    createLiElement(`執行 ${delayTime/1000} 秒後回傳計算值`)
     window.setTimeout(function(){
-      // 工作完成呼叫res函數結果透過參數傳遞進去
-      // resolve(n1 + n2) 
-      // reject(false)
-      console.log(n1 , n2)
+      resolve(n1 + n2) // 工作完成呼叫res函數結果透過參數傳遞進去
+      // reject(new Error(`失敗(rejected)`))
     }, delayTime)
   })
 }
 
-function test(){
+function clickPromiseThenCatch(){
   let promise = delayedAdd(3, 4, 2000)
-  console.log('test', promise)
-  // 接到 promise 物件回傳值
-  promise.then(function(res){
-    console.log(res)
-  }).catch(function(err){
-    console.log(err)
+  // 接到 promise 物件回傳值 .then
+  promise
+  .then(function(res){
+    createLiElement(res)
+    clearLi()
+  })
+  .catch(function(err){
+    createLiElement(err)
+    clearLi()
   })
 }
 
-function clickTest() {
-  console.log('clickTest')
+function clickPromiseAll() {
   let promise = delayedAdd(3, 4, 2000)
-  let promise2 = delayedAdd(3, 4, 2000)
+  let promise2 = delayedAdd(3, 4, 3000)
    // 多個 promise 處理完後，才繼續工作
-  Promise.all([promise, promise2]).then(function(res){
-    // console.log(res)
-    console.log(res[0] + res[1])
-  }).catch(function(err){
-    console.log(err)
+  Promise
+  .all([promise, promise2]).then(function(res){
+    createLiElement(res[0] + res[1])
+    clearLi()
+  })
+  .catch(function(err){
+    createLiElement(err)
+    clearLi()
   })
 }
 
 
 // async/await 簡化 promise 語法 
-async function hoverTest(){
+async function clickAsyncAwait(){
   try {
     console.log('hoverTest')
     // 前提 async的函數必須回傳 promise 物件
     let promise = await delayedAdd(4, 4, 3000)
-    console.log(promise)
+    createLiElement(promise)
+    clearLi()
     }catch (error) {
-    console.log(error)
+    createLiElement(error)
+    clearLi()
     }
 }
+
+window.clickAsyncAwait = clickAsyncAwait
+window.clickPromiseAll = clickPromiseAll
+window.clickPromiseThenCatch = clickPromiseThenCatch
