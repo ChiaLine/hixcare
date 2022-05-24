6061049c113911366e516f0c3dc42e4387f361a7
egg <egg810207@gmail.com>
2022-05-24 21:28:09 +0800

    Promise 煮飯應用、if 判斷 使用 throw 例外處理

---
+++ ability/node/02-Promise-then-catch/promiseTryCatch03.js
@@ -0,0 +1,32 @@
+// 煮飯
+const promise = new Promise((resolve, rejects) => {
+  setTimeout(() => {
+    try {
+      const number1 = Math.floor(Math.random() * 3) + 1 // 隨機米杯 1 ~ 3
+      const number2 = Math.floor(Math.random() * 3) + 1 // 隨機水杯 1 ~ 3
+      const result = (number1 === number2) ? { water: function(){ console.log(`水米比例完美：${number1}：${number2}`)} } : null 
+      result.water() // 隨機設定意外
+      
+      // if (number1 !== number2) {
+      //   // throw `丟給 catch 的參數錯誤值`
+      //   throw new Error(`水的比例不對 ${number1} : ${number2}`) // 例外處理陳述式
+      // }
+      
+      resolve(`成功把飯煮熟`)
+    } catch (e) {
+      rejects(`飯沒熟 ${e}`)
+    }
+  }, 1000)
+})
+
+promise.then((rse) => {
+  console.log(rse)
+  console.log("媽媽開始準備配菜")
+  console.log("準備開吃")
+})
+.catch((e) => {
+  console.log(e)
+  console.log("媽媽要重新煮飯")
+  console.log("晚餐時間延後")
+})
+
