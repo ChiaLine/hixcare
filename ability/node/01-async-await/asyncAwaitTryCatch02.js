e1c3426ab64925af161013fe3c5d899c4e50cba0
egg <egg810207@gmail.com>
2022-05-25 16:10:22 +0800

    新增 try catch ＋ 同時打包但 await等都打包完才搬家 ＆＆ 如果漏掉打包 就不能搬家了

--- ability/node/01-async-await/asyncAwaitTryCatch02.js
+++ ability/node/01-async-await/asyncAwaitTryCatch02.js
@@ -14,20 +14,19 @@ async function pack(area, time) {
 // try catch ＋ 同時打包但 await等都打包完才搬家 ＆＆ 如果漏掉打包 就不能搬家了
 async function home3() {
   try {
-    const pack1 = pack("廚房", 1000).catch((e) => e)
+    const pack1 = pack("廚房", 1000).catch((e) => e) // 新增 catch 存入 message
     const pack2 = pack("客廳", 2000).catch((e) => e)
     const pack3 = pack("房間", 500).catch((e) => e)
-    // console.log(pack1, pack2, pack3)
 
-    const ps = await Promise.all([pack1, pack2, pack3])
-    
+    const ps = await Promise.all([pack1, pack2, pack3]) // 等待所有的回傳值 再一起處理
+
     let message = '' 
     ps.forEach((p) => {
       if (p instanceof Error) {
         message += p.message + '\n'
       }
     })
-    if (message !== '') throw new Error(message)
+    if (message !== '') throw new Error(message) // 打包 所有的例外處理 丟給catch
     
     console.log(p1, '1')
     console.log(p2, '2')
@@ -36,7 +35,7 @@ async function home3() {
     console.log('順利結束大搬家!!')
 
   } catch(e) {
-    console.log(`${e} 搬家時間要往後延！！！`)
+    console.log(`${e} 搬家時間要往後延！！！`) // 接 throw 給的 message
   }
 }
 
@@ -71,68 +70,3 @@ async function home2() {
 }
 
 // home2()
-
-
-
-
-// 廚房打包 
-async function packKitchen() {
-  await new Promise(resolve => setTimeout(() => resolve(), 3000));
-  const number = Math.floor(Math.random() * 2) // 隨機 0 1
-
-  if (!number) {
-    throw new Error("忘記打包電鍋")
-  }
-
-  return "廚房順利打包"
-}
-
-// 客廳打包
-async function packParlour() {
-  await new Promise(resolve => setTimeout(() => resolve(), 3000));
-  const number = Math.floor(Math.random() * 2) // 隨機 0 1
-
-  if (!number) {
-    throw new Error("忘記收抱枕")
-  }
-
-  return "客廳順利打包"
-}
-
-// 房間打包
-async function packRoom() {
-  await new Promise(resolve => setTimeout(() => resolve(), 3000));
-  const number = Math.floor(Math.random() * 2) // 隨機 0 1
-
-  if (!number) {
-    throw new Error("忘記收枕頭")
-  }
-
-  return "房間順利打包"
-}
-
-// 使用 await 等待時間較長 每個 3秒後 ＝ 9
-async function home() {
-  const pack1 = await packKitchen().catch((e) => {
-    // console.log(e)
-    return `${e}，買新的`
-  })
-  console.log(pack1, '1')
-
-  const pack2 = await packParlour().catch((e) => {
-    // console.log(e)
-    return `${e}，買新的`
-  })
-  console.log(pack2, '2')
-
-  const pack3 = await packRoom().catch((e) => {
-    // console.log(e)
-    return `${e}，買新的`
-  })
-  console.log(pack3, '3')
-
-  console.log('順利大搬家!!')
-}
-
-// 使用 await 等待時間較長 每個 3秒後 ＝ 9
-// home() 
