4baf73d6a132ff8fba7a9bfd11934366e30de94d
egg <egg810207@gmail.com>
2022-06-04 10:32:29 +0800

    新增 undefined & null 情境

--- ability/node/06-object/object02-ObjectAssign.js
+++ ability/node/06-object/object02-ObjectAssign.js
@@ -8,13 +8,13 @@ console.log('-----複製-----------')
 // const destInst = Object.assign({}, srcInst);
 // console.log(JSON.stringify(destInst, null, 2));
 
-console.log('-----複製並且填滿預設值---- 寫死預設值 -----------')
+console.log('-----複製並且  寫死預設值 -----------')
 // const srcInst = { a: 1 };
 // const destInst = Object.assign({ a: 0, b: 0 }, srcInst); // ! key相同時 後者會覆蓋前者
 // console.log(JSON.stringify(destInst, null, 2));
 
 
-console.log('-----複製並且填滿預設值---- 模板預設值 -----------')
+console.log('-----複製並且 模板預設值 -----------')
 // const defaultInst = { a: 0, b: 0 };
 // const srcInst = { a: 1 };
 // const destInst = {};
@@ -25,44 +25,53 @@ console.log('-----複製並且填滿預設值---- 模板預設值 -----------')
 
 
 console.log('-----複製並且填滿預設值---- 錯誤示範 -----------')
-const defaultInst = { a: 0, b: 0 }; // 目標對象
-const srcInst = { a: 1 }; // 來源對象
-const destInst = Object.assign(defaultInst, srcInst); // ! 淺層 複製
+// const defaultInst = { a: 0, b: 0 }; // 目標對象
+// const srcInst = { a: 1 }; // 來源對象
+// const destInst = Object.assign(defaultInst, srcInst); // ! 淺層 複製
+// console.log(destInst);
+// console.log(srcInst);
+// console.log(defaultInst); // 值 會被覆蓋
+
+
+console.log('----- undefined & null 情境 希望被預設值替代 但無效 -----------')
+const defaultInst = { a: 0, b: 0 }; // 預設值
+const srcInst = { a: undefined };
+// const srcInst = { a: null };
+const destInst = Object.assign({}, defaultInst)
+Object.assign(destInst, srcInst)
+// Object.assign(srcInst, destInst)
 console.log(destInst);
-console.log(srcInst);
-console.log(defaultInst); // 值 會被覆蓋
 
 
+// console.log('----------- 物件 複製時 要注意 淺層 ＆ 深層 -----------')
+// function test() {
+//   let a = { b: { c: 4 }, d: { e: { f: 1 } } }
+//   let g = Object.assign({}, a) // 淺層 複製
+//   let h = JSON.parse(JSON.stringify(a)); // 深層 複製
+//   console.log(g.d) // { e: { f: 1 } }
+//   g.d.e = 32
+//   console.log('g.d.e set to 32.') // g.d.e set to 32.
+//   console.log(g) // { b: { c: 4 }, d: { e: 32 } }
+//   console.log(a) // { b: { c: 4 }, d: { e: 32 } }
+//   console.log(h) // { b: { c: 4 }, d: { e: { f: 1 } } }
+//   h.d.e = 54
+//   console.log('h.d.e set to 54.') // h.d.e set to 54.
+//   console.log(g) // { b: { c: 4 }, d: { e: 32 } }
+//   console.log(a) // { b: { c: 4 }, d: { e: 32 } }
+//   console.log(h) // { b: { c: 4 }, d: { e: 54 } }
+// }
+// test();
 
-console.log('----------- 物件 複製時 要注意 淺層 ＆ 深層 -----------')
-function test() {
-  let a = { b: { c: 4 }, d: { e: { f: 1 } } }
-  let g = Object.assign({}, a) // 淺層 複製
-  let h = JSON.parse(JSON.stringify(a)); // 深層 複製
-  console.log(g.d) // { e: { f: 1 } }
-  g.d.e = 32
-  console.log('g.d.e set to 32.') // g.d.e set to 32.
-  console.log(g) // { b: { c: 4 }, d: { e: 32 } }
-  console.log(a) // { b: { c: 4 }, d: { e: 32 } }
-  console.log(h) // { b: { c: 4 }, d: { e: { f: 1 } } }
-  h.d.e = 54
-  console.log('h.d.e set to 54.') // h.d.e set to 54.
-  console.log(g) // { b: { c: 4 }, d: { e: 32 } }
-  console.log(a) // { b: { c: 4 }, d: { e: 32 } }
-  console.log(h) // { b: { c: 4 }, d: { e: 54 } }
-}
-test();
 
-
-console.log('----------- 物件 複製時 要注意 淺層 ＆ 深層 -----------')
-const egg = { a: { b: 9 }, c: 1 }
-const eg = Object.assign({}, egg)
-const e = JSON.parse(JSON.stringify(eg, null))
-console.log(egg)
-console.log(eg)
-console.log(e)
-eg.c = 8
-eg.a.b = 13
-console.log(egg)
-console.log(eg)
-console.log(e)
+// console.log('----------- 物件 複製時 要注意 淺層 ＆ 深層 -----------')
+// const egg = { a: { b: 9 }, c: 1 }
+// const eg = Object.assign({}, egg)
+// const e = JSON.parse(JSON.stringify(eg, null))
+// console.log(egg)
+// console.log(eg)
+// console.log(e)
+// eg.c = 8
+// eg.a.b = 13
+// console.log(egg)
+// console.log(eg)
+// console.log(e)
