2eef7e209443ddea7bda72a0987c71fa71a15cc7
egg <egg810207@gmail.com>
2022-06-10 15:08:54 +0800

    投球 紀錄板 應用、第一版：父傳子，子改資料在傳父更新、第二版：父傳子，子傳遞通知請父進行改資料更新

--- vue/vue-v-model-property-emit/src/App.vue
+++ vue/vue-v-model-property-emit/src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>投球 紀錄板</h1>
+    <ScoreBoard :score="score" 
+    @after-add-hit="afterAddHit" 
+    @after-add-miss="afterAddMiss" 
+    @is-add-miss="isAddMiss" 
+    @is-add-hit="isAddHit" 
+    
+    />
+
+    <div class="add-hit-miss-content">
+      <p>hit: {{ score.hit }}</p>
+      <p>shoot: {{ score.shoot }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ScoreBoard from './components/ScoreBoard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    ScoreBoard
+  },
+  data(){
+    return {
+      score: {
+        hit: 7,
+        shoot: 10
+      }
+    }
+  },
+  methods: {
+    // 第一版 接 子元件傳遞資料 修改 父元件 data
+    afterAddMiss(shoot){
+      console.log("afterAddMiss", shoot)
+      this.score.shoot = shoot
+    },
+    afterAddHit(scoreBoard){
+      console.log('afterAddHit', scoreBoard)
+      this.score = scoreBoard
+    },
+    // 第二版 接 子元件觸發通知 父元件才修改資料
+    isAddMiss(){
+      this.score.shoot++
+    },
+    isAddHit(){
+      this.score.shoot++
+      this.score.hit++
+    }
   }
 }
 </script>
@@ -24,5 +66,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.add-hit-miss-content {
+  width: 600px;
+  height: 60px;
+  margin: 10px auto;
+  padding: 10px;
+  border-radius: 25px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+p {
+  border: 2px solid #000;
+  border-radius: 25px;
+  width: 45%;
+  font-size: 35px;
 }
 </style>
