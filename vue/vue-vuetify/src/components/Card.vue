d44e460a2883d64d89cc48d271a50b6176fd7324
egg <egg810207@gmail.com>
2022-06-13 19:04:57 +0800

    新增 <template lang="pug"> pug 模板寫法

---
+++ vue/vue-vuetify/src/components/Card.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+v-card.mx-auto(max-width='344')
+  v-img(src='https://cdn.vuetifyjs.com/images/cards/sunshine.jpg', height='200px')
+  v-card-title  Top western road trips 
+  v-card-subtitle  1,000 miles of wonder 
+  v-card-actions
+    v-btn(color='orange lighten-2', text='')  Explore 
+    v-spacer
+    v-btn(icon='', @click='show = !show')
+      v-icon {{ show ? &quot;mdi-chevron-up&quot; : &quot;mdi-chevron-down&quot; }}
+  v-expand-transition
+    div(v-show='show')
+      v-divider
+      v-card-text
+        | I&apos;m a thing. But, like most politicians, he promised more than he
+        | could deliver. You won&apos;t have time for sleeping, soldier, not with all
+        | the bed making you&apos;ll be doing. Then we&apos;ll go with that data file!
+        | Hey, you add a one and two zeros to that or we walk! You&apos;re going to
+        | do his laundry? I&apos;ve got to find a way to escape.
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+};
+</script>
---
+++ vue/vue-vuetify/src/components/Carousel.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+v-carousel(v-model='model')
+  v-carousel-item(v-for='(color, i) in colors', :key='color')
+    v-sheet(:color='color', height='100%', tile='')
+      v-row.fill-height(align='center', justify='center')
+        .text-h2
+          | Slide {{ i + 1 }}
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      model: 0,
+      colors: [
+        'primary',
+        'secondary',
+        'yellow darken-2',
+        'red',
+        'orange',
+      ],
+    }),
+  }
+</script>
