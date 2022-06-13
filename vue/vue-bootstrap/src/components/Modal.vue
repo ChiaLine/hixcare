50d14a8041de82943935f7dde4d7e4bf41c80a2a
egg <egg810207@gmail.com>
2022-06-13 16:36:10 +0800

    彈跳視窗 使用ID綁定 v-b-modal="'modal-1'"

---
+++ vue/vue-bootstrap/src/components/Modal.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <!-- modal 使用綁定id名稱 v-b-modal="'modal-1'" -->
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+  </div>
+</template>
