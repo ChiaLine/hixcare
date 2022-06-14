b8e01c7595403ae793d3af9a68511f5a839b9cc8
egg <egg810207@gmail.com>
2022-06-14 18:02:41 +0800

    深淺色主題模式 設定切換

--- vue/vue-vuetify/src/App.vue
+++ vue/vue-vuetify/src/App.vue
@@ -1,25 +1,51 @@
 <template lang="pug">
+  //- 最外層 v-app
   v-app
-    v-app-bar(app color='primary' dark)
-      .d-flex.align-center
-        v-img.shrink.mr-2(alt='Vuetify Logo' contain src='https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png' transition='scale-transition' width='40')
-          v-img.shrink.mt-1.hidden-sm-and-down(alt='Vuetify Name' contain min-width='100' src='https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png' width='100').
-            
-            
+    //- 導覽設定 v-app-bar(bottom='', color='primary', dark='')
+      位置 app/bottom 主題顏色內primary key名稱 深色模式dark
+    v-app-bar(app='')
+      v-toolbar-items
+        v-list-item
+          v-icon.mr-2 mdi-account-box-multiple
+          //- <v-icon class="mr-2"> mdi-account-box-multiple </v-icon>
+          span.primary--text.font-weight-bold.title Employee
+          span.primary--text.font-weight-light.title Data
+          v-list-item-content
+      //- v-spacer 用于分配父子组件之间的剩余宽度
       v-spacer
-      v-btn(href='https://github.com/vuetifyjs/vuetify/releases/latest' target='_blank' text)
-        span.mr-2 Latest Release
-        v-icon mdi-open-in-new
+      v-toolbar-items
+        //- 切換按鈕顏色元件
+        ThemeChangerMenu
     v-main
-      Carousel
-      card
+      //- Carousel
+      card.mt-10
       //- navbar
+      v-list-item-action
+      v-switch(v-model='$vuetify.theme.dark')
+      //- Carousel
+
+      //- 導覽列
+      v-app-bar(bottom='')
+        //- 标题
+        v-toolbar-title
+          //- 圖示
+          v-icon.mr-2 mdi-account-box-multiple
+          //- 文字 info螢光色
+          span Employee
+          //- 文字 info文字色
+          span Data
+        //- 用于 分配父子组件之间的剩余宽度
+        v-spacer(color="indigo darken-3")
+        //- 切換按鈕顏色元件
+        ThemeChangerMenu
+        
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import Carousel from "./components/Carousel.vue";
+import ThemeChangerMenu from "./components/ThemeChangerMenu.vue";
 
 export default {
   name: "App",
@@ -27,7 +53,8 @@ export default {
   components: {
     Navbar,
     Card,
-    Carousel
+    Carousel,
+    ThemeChangerMenu
   },
 
   data: () => ({
--- vue/vue-vuetify/src/plugins/vuetify.js
+++ vue/vue-vuetify/src/plugins/vuetify.js
@@ -4,4 +4,36 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  //? 深色切換 可通过 this.$vuetify.theme.dark 控制
+  theme: { 
+    // ? 默认使用 light主题，可在主题服务中添加 dark选项修改
+    dark: false, 
+    //? disable禁用主題
+    // disable: true,
+    //? 主題顏色設定修改
+    themes: {
+      dark: {
+        primary: "#fff", // ? 主要
+        accent: "#FFCA28", // ? 強調
+        secondary: "#607D8B", // ? 次要
+        success: "#FFEB3B", // ? 成功
+        info: "#2196F3", // ? 信息
+        warning: "#9C27B0", // ? 警告
+        error: "#B71C1C" // ? 錯誤
+      },
+      //? 淺色設定
+      light: {
+        primary: "#3f51b5",
+        accent: "#673ab7",
+        secondary: "#9c27b0",
+        success: "#e91e63",
+        info: "#f44336",
+        warning: "#4caf50",
+        error: "#009688"
+      }
+    },
+  },
 });
+
+
+
