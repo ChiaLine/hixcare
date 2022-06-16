3bd49365ea89318a88db8d799b40d25ac1df35b4
egg <egg810207@gmail.com>
2022-06-16 11:40:51 +0800

    新增  keydown 事件 正常觸發模式

--- vue/vue-keydown/src/App.vue
+++ vue/vue-keydown/src/App.vue
@@ -3,7 +3,19 @@
     //-  <img alt="Vue logo" src="./assets/logo.png"> 
     //-  <HelloWorld msg="Welcome to Your Vue.js App"/> 
     h1 做一個只能輸入整數的 input 元件
-    
+    h1 keydown 事件研究 触发于键盘按键按下的时候
+    div
+      //- input(type='text', placeholder='請輸入', @keydown='show($event)')
+
+    div
+      label(for='name') 輸入 正常觸發模式：
+      //- input 綁定事件觸發 正常觸發模式
+      input#name(type='text', placeholder='請輸入' @keydown='show($event)')
+
+    div
+      label(for='name1') 模式：
+      //- 模式
+      input#name1(type='text',  placeholder='請輸入' @keydown='show($event)')
 </template>
 
 <script>
@@ -13,6 +25,11 @@ export default {
   name: 'App',
   components: {
     // HelloWorld
+  },
+  methods: {
+    show(event) {
+      console.log(event)
+    }
   }
 }
 </script>
@@ -25,5 +42,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+div {
+  margin-bottom: 15px;
+}
+
+label{
+  font-size:  26px;
+}
+
+input {
+  height: 24px;
 }
 </style>
